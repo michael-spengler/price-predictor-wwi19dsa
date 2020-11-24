c5b8df67bfa45b20b96971fb710d962970d89819
Ronho <rh.com>
2020-11-24 14:49:18 +0100

    toolbar setup

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 const MaterialComponents = [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
 ];
 
 @NgModule({
