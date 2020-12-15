3d24e380304bdbcfc6e83897008d5a5e71ad5741
Ferdi Muth <61280761+pfeeerdi@users.noreply.github.com>
2020-12-16 00:34:47 +0100

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiEndpoint: '3.131.4.23:5000/'
 };
 
 /*
