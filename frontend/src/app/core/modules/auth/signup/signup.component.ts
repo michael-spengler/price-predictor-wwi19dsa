2498951e2caa71cfc10c7197ebbd8efbf19b223c
Bastian Berle <bastian.berle@gmail.com>
2020-12-16 12:18:40 +0100

    created simple input fields for signup

--- frontend/src/app/core/modules/auth/signup/signup.component.ts
+++ frontend/src/app/core/modules/auth/signup/signup.component.ts
@@ -1,33 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Validators, FormGroup, FormBuilder } from '@angular/forms'; //FormControl,
-//import { AuthenticationService } from '../../../services/authentication/authentication.service';
+import { Component } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../../services/authentication/authentication.service';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
   public hide = true;
-  public signupForm: FormGroup | undefined;
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
-  constructor(
-    private _snackBar: MatSnackBar,
-    //private router: Router,
-    private formBuilder: FormBuilder
-  ) { }
+  public signupForm: FormGroup = this.formBuilder.group({
+    email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    username: [null, Validators.required],
+    password: [null, Validators.required],
+    repeatedPassword: [null, Validators.required],
+  });
 
-  ngOnInit(): void {
-    // this.signupForm = this.formBuilder.group({
-    //   email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
-    //   password: [null, Validators.required]
-    // });
-  }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
