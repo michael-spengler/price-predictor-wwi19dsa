e128b64f39f71fec5e17bdb758f274f0ad5a82d5
Bastian Berle <bastian.berle@gmail.com>
2020-12-16 12:17:32 +0100

    fixed object undefined error + Validation

--- frontend/src/app/core/modules/auth/login/login.component.ts
+++ frontend/src/app/core/modules/auth/login/login.component.ts
@@ -1,38 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { AuthenticationService } from '../../../services/authentication/authentication.service';
+import { Component } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../../services/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-
-  public hide = true;
-  public loginForm: FormGroup | undefined;
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-
+  public hide = true;
+  public loginForm: FormGroup = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
+      password: [null, Validators.required]
+  });
 
   constructor(
     private authenticationService: AuthenticationService,
     private _snackBar: MatSnackBar,
     private router: Router,
     private formBuilder: FormBuilder
-  ) {
-    this.loginForm = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
-      password: [null, Validators.required]
-    });
-  }
-
-  ngOnInit(): void {
-
-  }
+  ) {}
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
