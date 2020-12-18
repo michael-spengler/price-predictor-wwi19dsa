7f3d88997f8740b6d09ccc26e86a272db876bc02
femu <femu@MacBook-Pro.local>
2020-12-18 12:14:38 +0100

    fixed SignUp in API

--- API/server.py
+++ API/server.py
@@ -4,6 +4,10 @@ from functions import createHashAndSalt, matchingPassword, createToken, verifyTo
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from flask_cors import CORS, cross_origin
+import sys
+
+
+
 
 
 app = Flask(__name__)
@@ -12,20 +16,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tmp/database.db"
 db = SQLAlchemy(app)
 CORS(app, support_credentials=True)
 
+
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False)
+    lastName = db.Column(db.String, nullable=False)
     firstName = db.Column(db.String, nullable=False)
-    street = db.Column(db.String, nullable=False)
-    plz = db.Column(db.Integer, nullable=False)
-    city = db.Column(db.String, nullable=False)
-    country = db.Column(db.String, nullable=False)
+    street = db.Column(db.String, nullable=True)
+    zip = db.Column(db.Integer, nullable=True)
+    city = db.Column(db.String, nullable=True)
+    country = db.Column(db.String, nullable=True)
     password = db.Column(db.LargeBinary, nullable=False)
+    birthdate = db.Column(db.String, nullable=False)
 
     def _repr_(self):
-        return f"id : {id}, email : {email}, username : {username}, firstName : {firstName}, name : {name}, street : {street}, plz : {plz}, city : {city}, country : {country}"
+        return f"id : {id}, email : {email}, username : {username}, firstName : {firstName}, name : {name}, street : {street}, plz : {plz}, city : {city}, country : {country}, birtdate : {birtdate}"
 
 resource_fields = {
     'id': fields.String,
@@ -34,9 +40,10 @@ resource_fields = {
     'name': fields.String,
     'firstName': fields.String,
     'street': fields.String,
-    'plz': fields.Integer,
+    'zip': fields.Integer,
     'city': fields.String,
     'country': fields.String,
+    'birtdate': fields.String
 }
 
 #db.drop_all()
@@ -48,10 +55,17 @@ user_signin_args.add_argument("email", type=str, help="email missing", required=
 user_signin_args.add_argument("password", type=str, help="password missing", required=True)
 
 user_signup_args = reqparse.RequestParser()
-user_signup_args.add_argument("email", type=str, help="email missing", required=True)
 user_signup_args.add_argument("username", type=str, help="username missing", required=True)
-user_signup_args.add_argument("name", type=str, help="name missing", required=True)
+user_signup_args.add_argument("email", type=str, help="email missing", required=True)
 user_signup_args.add_argument("password", type=str, help="password missing", required=True)
+user_signup_args.add_argument("firstName", type=str, help="firstName missing", required=True)
+user_signup_args.add_argument("lastName", type=str, help="lastName missing", required=True)
+user_signup_args.add_argument("street", type=str, help="street missing", required=False)
+user_signup_args.add_argument("zip", type=int, help="zip missing", required=False)
+user_signup_args.add_argument("country", type=str, help="country missing", required=False)
+user_signup_args.add_argument("birthdate", type=str, help="birthdate missing", required=False)
+
+
 
 
 user_args = reqparse.RequestParser()
@@ -63,7 +77,7 @@ class SignIn(Resource):
     @cross_origin(supports_credentials=True)
     def post(self):
         args = user_signin_args.parse_args() #reading args from json
-        result = UserModel.query.filter_by(email=args.email).first()
+        args.email = args.email.lower()
 
         #Test Case
         if args.email == 'demo@test.de' and args.password == "Test1":
@@ -71,6 +85,7 @@ class SignIn(Resource):
             return {"message" : "Successfully authenticated"}, 201, {"Authorization" :  token, "Access-Control-Expose-Headers": "Authorization"},  #body, status, header
         #End of Test Case 
 
+        result = UserModel.query.filter_by(email=args.email).first()
         if not result:
             abort(401, message="Email or Password incorrect")
         storedKey = result.password #loading stored Key
@@ -81,16 +96,19 @@ class SignIn(Resource):
             return {"message" : "Successfully authenticated"}, 201, {"Authorization" :  token, "Access-Control-Expose-Headers": "Authorization"},  #body, status, header
 
 class SignUp(Resource):
-
+    @cross_origin(supports_credentials=True)
     @marshal_with(resource_fields)
     def post(self):
+        #load body
         args = user_signup_args.parse_args()
+        args.email = args.email.lower()
+        #check result
         result = UserModel.query.filter_by(email=args.email).first()
         if result:
             abort(409, message="Email is taken...")
         hashedPassword = createHashAndSalt(args.password)
-        print(str(hashedPassword))
-        user = UserModel(email=args.email, password=hashedPassword, username=args.username, name=args.name)
+        user = UserModel(email=args.email, password=hashedPassword, username=args.username, \
+            firstName=args.firstName, lastName=args.lastName, country=args.country, zip=int(args.zip), street=args.street, birthdate=args.birthdate)
         db.session.add(user)
         db.session.commit()
         return user, 201
@@ -109,4 +127,6 @@ api.add_resource(CheckToken, "/checkToken")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    hostip = sys.argv[1]
+    debugmode = bool(sys.argv[2])
+    app.run(debug=debugmode, host=hostip)
