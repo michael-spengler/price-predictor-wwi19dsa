416601cde0df214c86b0c58fd5c93fe9d911585d
Fabian Heidger <Fabian.Heidger@va-it-office.de>
2020-12-29 14:23:21 +0100

    Add About module

---
+++ frontend/src/app/modules/about/us/us.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsComponent } from './us.component';
+
+describe('UsComponent', () => {
+  let component: UsComponent;
+  let fixture: ComponentFixture<UsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
