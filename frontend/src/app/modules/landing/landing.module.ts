2fcedcff8b7a4cac629983117169ca1d87a681be
Fabian Heidger <Fabian.Heidger@va-it-office.de>
2021-01-03 19:12:26 +0100

    Add Landing Page

--- frontend/src/app/modules/landing/landing.module.ts
+++ frontend/src/app/modules/landing/landing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { LandingRoutingModule } from './landing-routing.module';
-
+import { MaterialModule } from '../../material.module';
 
 
 @NgModule({
   declarations: [LandingComponent],
   imports: [
     CommonModule,
-    LandingRoutingModule
+    LandingRoutingModule,
+    MaterialModule
   ]
 })
 export class LandingModule { }
