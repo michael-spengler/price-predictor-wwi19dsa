d7885ac0a31c7ba6ffb677a869956144dc599946
Ron <ron@h.com>
2021-02-09 20:59:28 +0100

    trade dialog layout

--- frontend/src/app/shared/components/trade-dialog/trade-dialog.component.ts
+++ frontend/src/app/shared/components/trade-dialog/trade-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Trade } from '../../models/trade.model';
 
 @Component({
   selector: 'app-trade-dialog',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trade-dialog.component.scss']
 })
 export class TradeDialogComponent implements OnInit {
+
+  isLoading: Boolean = false;
+
+  trade: Trade = {
+    "type": 'sell',
+    "date": new Date,
+    "motivation": 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    "description": 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    "expectedIncrease": 100,
+    "percent": 12,
+    "startdate": new Date,
+    "enddate": new Date,
+    "fiatcurrency": 'USD',
+    "cryptocurrency": 'BTC',
+    "author": 'test'
+  }
 
   constructor() { }
 
