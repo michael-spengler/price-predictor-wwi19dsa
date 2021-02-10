c16d5207147f2abde4db4aeb4491966b7d6b35dd
Ron <ron@h.com>
2021-02-10 12:47:54 +0100

    add blog post services and export methods

---
+++ frontend/src/app/shared/services/blog-post/blog-post.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { BlogPost } from '../../models/blog-post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllPosts(): Observable<BlogPost[]> {
+    return this.httpClient.get(environment.apiEndpoint + 'blog').pipe(
+      retry(2),
+      map((data: any) => {
+        return data.data.filter((post: BlogPost) => {
+          try {
+            let date = post.date ? post.date : '';
+            post.date = new Date(date);
+            if (!isNaN(post.date.getTime())) {
+              return true
+            } else {
+              return false;
+            }
+          } catch (error) {
+            return false
+          }
+        }
+        );
+      })
+    );
+  }
+
+  public getPostById(id: number): Observable<BlogPost> {
+    return this.httpClient.get<BlogPost>(environment.apiEndpoint + 'blog/' + id.toString()).pipe(
+      retry(2),
+      map((post: BlogPost) => {
+        let date = post.date ? post.date : '';
+        post.date = new Date(date);
+        if (isNaN(post.date.getTime())) {
+          post.date = new Date();
+        }
+        return post
+      })
+    );
+  }
+}
