5f6370fe9603174004ceb0015ecbd1e01c61fbc9
Ron <ron@h.com>
2021-02-10 23:42:37 +0100

    add trade service

---
+++ frontend/src/app/shared/services/trade/trade.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Trade } from '../../models/trade.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TradeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllTrades(): Observable<Trade[]> {
+    return this.httpClient.get(environment.apiEndpoint + 'trade').pipe(
+      retry(2),
+      map((data: any) => {
+        return data.data.filter((trade: Trade) => {
+          try {
+            trade.date = this.parseToDate(trade.date);
+            trade.startdate = this.parseToDate(trade.startdate);
+            trade.enddate = this.parseToDate(trade.enddate);
+
+            if (!isNaN(trade.date.getTime())) {
+              return true
+            } else {
+              return false;
+            }
+          } catch (error) {
+            return false
+          }
+        }
+        );
+      })
+    );
+  }
+
+  private parseToDate(dateString: any): Date {
+    let date = dateString ? dateString : '';
+    return new Date(date);
+  }
+}
