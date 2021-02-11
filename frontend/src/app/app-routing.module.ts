6b7817ab7339e60c458079f0aa048b4d00a184f8
Fabian Heidger <Fabian.Heidger@va-it-office.de>
2021-02-11 15:12:30 +0100

    F: changes Landingpage and Aboutus Page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { SidenavComponent } from './template/sidenav/sidenav.component';
 
 const routes: Routes = [
   {
-    path: 'landing',
-    loadChildren: () => import('./modules/landing/landing.module')
-      .then(m => m.LandingModule)
-  },
-  {
     path: '',
     component: SidenavComponent,
     loadChildren: () => import('./modules/application/application.module')
