721638a2fb18eaebfb3f3c92dff05a1f3bd7803b
Ron <ron@h.com>
2021-02-22 23:40:43 +0100

    i18n improvements and translations

--- frontend/src/app/modules/landing/landing/landing.component.ts
+++ frontend/src/app/modules/landing/landing/landing.component.ts
@@ -11,8 +11,8 @@ import { ThemeService } from 'src/app/shared/services/theme/theme.service';
 export class LandingComponent implements OnInit {
 
   links = [
-    {loc: '/feed', title: 'Blog'},
-    {loc: '/about/us', title: 'About Us'},
+    {loc: '/feed', title: $localize`:Feed@@feedID:Feed`},
+    {loc: '/about/us', title: $localize`:About Us@@aboutUsID:About Us`},
  ];
 
   constructor(
