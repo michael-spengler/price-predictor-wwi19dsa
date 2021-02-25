3afe45d72b696c34f2e0abcbfd0f20cb610c68ec
Ron <ron@h.com>
2021-02-25 21:13:07 +0100

    trade only shows acronyms of currencies

--- frontend/src/app/shared/components/trade/trade.component.ts
+++ frontend/src/app/shared/components/trade/trade.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Trade } from '../../models/trade.model';
 import { MatDialog } from '@angular/material/dialog';
 import { TradeDialogComponent } from '../trade-dialog/trade-dialog.component';
@@ -8,16 +8,28 @@ import { TradeDialogComponent } from '../trade-dialog/trade-dialog.component';
   templateUrl: './trade.component.html',
   styleUrls: ['./trade.component.scss']
 })
-export class TradeComponent {
+export class TradeComponent implements OnInit {
 
   @Input() trade = <Trade>{};
 
   constructor(private dialog: MatDialog) { }
 
+  ngOnInit() {
+    this.setCurrencyAcronyms();
+  }
+  
   openDialog() {
     this.dialog.open(TradeDialogComponent, {
       data: this.trade.id
     });
+  }
+
+  private setCurrencyAcronyms() {
+    const reg = /\(([^)]+)\)/;
+    const fiat_currency= reg.exec(this.trade.fiatcurrency);
+    const crypto_currency = reg.exec(this.trade.cryptocurrency);
+    this.trade.fiatcurrency = fiat_currency ? fiat_currency[1] : this.trade.fiatcurrency;
+    this.trade.cryptocurrency = crypto_currency ? crypto_currency[1] : this.trade.cryptocurrency;
   }
 
 }
